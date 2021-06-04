948ff2e1dac234ae06f848eea9b06890c5b32446
Vinicius Marques <viniciusmarques@ipe.digital>
2021-06-03 23:41:00 -0300

    adjusting users functional tests

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,17 +1,68 @@
+import AuthService from "@src/services/auth";
+
 describe('User functional tests', () => {
-  it('should return a list of users', async () => {
-    const { body, status } = await global.testRequest.get('/users');
-    expect(status).toBe(200);
-    expect(body).toEqual([
-      {
-        email: 'usuariocomum@teste.com.br',
-        accessLevel: 'funcionario',
-      },
-      {
-        email: 'usuarioadm@teste.com.br',
-        accessLevel: 'administrador',
-      },
-    ]);
+  describe('When user is unauthenticated', () => {
+    it('should return an unauthenticated error', async () => {
+      const { body, status } = await global.testRequest
+        .get('/health-check');
+      expect(status).toBe(401);
+      expect(body).toEqual({
+        code: 401,
+        message: "jwt must be provided",
+        name: "InvalidTokenError",
+      });
+    });
+  });
+
+  describe('When user is authenticated as funcionario', () => {
+    const defaultUser = {
+      email: 'usuariocomum@teste.com.br',
+      accessLevel: 'funcionario',
+    };
+
+    let token: string;
+
+    beforeEach(async () => {
+      token = AuthService.generateToken(defaultUser);
+    });
+
+    it('should return an unauthorized error', async () => {
+      const { body, status } = await global.testRequest.get('/users').set({ 'x-access-token': token });
+      expect(status).toBe(403);
+      expect(body).toEqual({
+        code: 403,
+        name: 'PermissionDeniedError',
+        message: 'Permission denied',
+      });
+    });
+  });
+
+  describe('When user is authenticated as administrador', () => {
+    const defaultUser = {
+      email: 'usuarioadm@teste.com.br',
+      accessLevel: 'administrador',
+    };
+
+    let token: string;
+
+    beforeEach(async () => {
+      token = AuthService.generateToken(defaultUser);
+    });
+
+    it('should return a list of users', async () => {
+      const { body, status } = await global.testRequest.get('/users').set({ 'x-access-token': token });
+      expect(status).toBe(200);
+      expect(body).toEqual([
+        {
+          email: 'usuariocomum@teste.com.br',
+          accessLevel: 'funcionario',
+        },
+        {
+          email: 'usuarioadm@teste.com.br',
+          accessLevel: 'administrador',
+        },
+      ]);
+    });
   });
 
   describe('When authenticating a user', () => {
