857bc17d082d50023aeb1e3c3c56a59e51a6a440
Vinicius Marques <viniciusmarques@ipe.digital>
2021-06-03 23:31:52 -0300

    adjusting health-check functional tests

--- test/functional/health-check.test.ts
+++ test/functional/health-check.test.ts
@@ -1,9 +1,59 @@
 import healthNormalizedFixture from '@test/fixtures/health_normalized_fixture.json';
+import AuthService from "@src/services/auth";
 
 describe('Health check functional tests', () => {
-  it('should a health normalized data', async () => {
-    const { body, status } = await global.testRequest.get('/health-check');
-    expect(status).toBe(200);
-    expect(body).toEqual(healthNormalizedFixture);
+  describe('When user is unauthenticated', () => {
+    it('should return an unauthenticated error', async () => {
+      const { body, status } = await global.testRequest
+        .get('/health-check');
+      expect(status).toBe(401);
+      expect(body).toEqual({
+          code: 401,
+          message: "jwt must be provided",
+          name: "InvalidTokenError",
+      });
+    });
+  });
+
+  describe('When user is authenticated as funcionario', () => {
+    const defaultUser = {
+      email: 'usuariocomum@teste.com.br',
+      accessLevel: 'funcionario',
+    };
+
+    let token: string;
+
+    beforeEach(async () => {
+      token = AuthService.generateToken(defaultUser);
+    });
+
+    it('should a health normalized data', async () => {
+      const { body, status } = await global.testRequest
+        .get('/health-check')
+        .set({ 'x-access-token': token });
+      expect(status).toBe(200);
+      expect(body).toEqual(healthNormalizedFixture);
+    });
+  });
+
+  describe('When user is authenticated as administrador', () => {
+    const defaultUser = {
+      email: 'usuarioadm@teste.com.br',
+      accessLevel: 'administrador',
+    };
+
+    let token: string;
+
+    beforeEach(async () => {
+      token = AuthService.generateToken(defaultUser);
+    });
+
+    it('should a health normalized data', async () => {
+      const { body, status } = await global.testRequest
+        .get('/health-check')
+        .set({ 'x-access-token': token });
+      expect(status).toBe(200);
+      expect(body).toEqual(healthNormalizedFixture);
+    });
   });
 });
